2a957591d9947b20fd84abf54b2d643cf8535d4b
ThiagoT18 <thiagoteixeiralima10@gmail.com>
2025-07-26 13:33:46 -0300

    Corrigido menu hambúrguer e ajustes finais

--- app/public/wp-content/themes/darkside-theme/header.php
+++ app/public/wp-content/themes/darkside-theme/header.php
@@ -46,7 +46,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'darkside-theme' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span>&#9776;</span> <!-- símbolo de menu hambúrguer --></button>
 			<?php
 			wp_nav_menu(
 				array(
--- app/public/wp-content/themes/darkside-theme/template-parts/contact-form.php
+++ app/public/wp-content/themes/darkside-theme/template-parts/contact-form.php
@@ -2,7 +2,7 @@
   <div class="container">
     <h2 data-aos="fade-down">Entre em Contato</h2>
     <div data-aos="zoom-in" data-aos-delay="150">
-      <?php echo do_shortcode('[contact-form-7 id="5d3540e" title="Untitled"]'); ?>
+      <?php echo do_shortcode('[contact-form-7 id="be27d46" title="Contact form 1"]'); ?>
     </div>
   </div>
 </section>
